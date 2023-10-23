e2b1fde7f34490f9396521d8c7f84c7ae4ac272a
Correa <tocr@gft.com>
2023-10-23 11:19:19 -0300

    Cosmetic chages

--- main.py
+++ main.py
@@ -3,32 +3,31 @@ from tkinter import *
 # Create window
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=500, height=300)
-window.config(padx=10, pady=10)
+window.config(padx=20, pady=20)
 
 #Input:
-input = Entry(width=50)
+input = Entry(width=15)
 input.grid(column=1, row=0)
 
 # Label: "Miles"
-label_02 = Label(text="Miles", font=("Arial", 24, "bold"))
+label_02 = Label(text="Miles", font=("Arial", 12, "bold"))
 label_02.grid(column=2, row=0)
-label_02.config(padx=50, pady=50)
+label_02.config(padx=5, pady=5)
 
 # Label: "is equal to"
-label_03 = Label(text="is equal to", font=("Arial", 24, "bold"))
+label_03 = Label(text="is equal to", font=("Arial", 12, "bold"))
 label_03.grid(column=0, row=1)
-label_03.config(padx=50, pady=50)
+label_03.config(padx=5, pady=5)
 
 # Output
-output = Label(text="000", font=("Arial", 24, "bold"))
+output = Label(text="000", font=("Arial", 12))
 output.grid(column=1, row=1)
-output.config(padx=50, pady=50)
+output.config(padx=5, pady=5)
 
 # Label: "Km"
-label_04 = Label(text="Km", font=("Arial", 24, "bold"))
+label_04 = Label(text="Km", font=("Arial", 12, "bold"))
 label_04.grid(column=2, row=1)
-label_04.config(padx=50, pady=50)
+label_04.config(padx=5, pady=5)
 
 # Button
 def button_clicked():
@@ -38,7 +37,7 @@ def button_clicked():
 
 button = Button(text="Calculate", command=button_clicked)
 button.grid(column=1, row=2)
-button.config(padx=50, pady=50)
+button.config(padx=5, pady=5)
 
 
 window.mainloop()
